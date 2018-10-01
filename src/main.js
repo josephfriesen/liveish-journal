e22db284bced0bd882b4a8c46c63653fcad2c2bd
Joe Friesen and Talia Haller <joe@>
2018-10-01 11:49:04 -0700

    complete full JS journal entry project.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
 import './styles.scss';
-import { pingPong } from './ping-pong';
+import { Entry } from './ping-pong';
+// import { pingPong } from './ping-pong';
+
+var allEntries = [];
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  $("#entry-form").submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    console.log("hellow");
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    let subject = $("#subject").val();
+    let body = $("#body").val();
+    var journalEntry = new Entry(subject,body);
+    allEntries.push(journalEntry);
+    journalEntry.entryNumber = allEntries.length;
+    $(".journal-container").prepend("<div class='entry-display' id='entry-" + journalEntry.entryNumber + "'></div>");
+    $(".entry-display:first").append(
+        "<h2>" + journalEntry.subject + "</h2>" +
+        "<h4 class='teaser'>" + journalEntry.getTeaser() + "</h4>" +
+        "<p class='entry-body hidden'>" + journalEntry.body + "</p>" +
+        "<p class='entry-body hidden meta-data'> Entry Number: " + journalEntry.entryNumber + "</p>" +
+        "<p class='entry-body hidden meta-data'> Number of Words: " + journalEntry.wordCount() + "</p>" +
+        "<p class='entry-body hidden meta-data'> Vowels: " + journalEntry.vowelCount() + "</p>" +
+        "<p class='entry-body hidden meta-data'> Consonants: " + journalEntry.consonantCount() + "</p>" +
+        "<button type='button' class='btn btn-default expand-button'>Expand Entry</button>"
+      );
+      $(".expand-button:first").click(function() {
+        $(this).siblings(".teaser, .entry-body").toggleClass("hidden");
+        $(this).siblings(".meta-data").toggleClass("inline");
+      });
   });
 });
--- src/ping-pong.js
+++ src/ping-pong.js
@@ -1,15 +1,54 @@
-export function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
+// export function pingPong(goal) {
+//   var output = [];
+//   for (var i = 1; i <= goal; i++) {
+//     if (i % 15 === 0) {
+//       output.push("ping-pong");
+//     } else if (i % 3 === 0) {
+//       output.push("ping");
+//     } else if (i % 5 === 0) {
+//       output.push("pong");
+//     } else  {
+//       output.push(i);
+//     }
+//   }
+//   return output;
+// }
+
+export function Entry(subject, body) {
+  this.subject = subject;
+  this.body = body;
+  this.bodyArr = body.split(" ");
+  this.entryNumber;
+}
+
+Entry.prototype.wordCount = function() {
+  const len = this.bodyArr.length;
+  return len;
+};
+
+Entry.prototype.vowelCount = function() {
+  var vowelArr = this.body.split(/[aeiou]/i);
+  var vowelLen = vowelArr.length - 1;
+  return vowelLen;
+};
+
+Entry.prototype.consonantCount = function() {
+  var consArr = this.body.split(/[bcdfghjklmnpqrstvwxyz]/i);
+  var consLen = consArr.length - 1;
+  return consLen;
+};
+
+Entry.prototype.getTeaser = function() {
+  var truncArr = [];
+  // var stop = 7;
+  var newWord;
+  for (var i = 0; i < 8; i++) {
+    newWord = this.bodyArr[i];
+    truncArr.push(newWord);
+    if (/[.?!]/.test(newWord)) {
+      return truncArr.join(' ');
     }
   }
-  return output;
-}
+  truncArr.push('...');
+  return truncArr.join(' ');
+};
